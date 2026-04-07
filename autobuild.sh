19e8a8b2d2a45adde9db8f13a9f1643fd27e6c72
rx <842722738@qq.com>
2026-04-07 20:55:24 +0800

    first change

--- autobuild.sh
+++ autobuild.sh
@@ -6,6 +6,10 @@ mkdir -p build
 
 rm -rf $(pwd)/build/*
 
+protoc --cpp_out=$(pwd)/src/ $(pwd)/src/rpcheader.proto
+
+mv $(pwd)/src/rpcheader.pb.h $(pwd)/src/include/
+
 cd $(pwd)/build &&
     cmake .. &&
     make
